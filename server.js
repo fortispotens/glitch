32e8ef2846adb5f4a14f4cd8eb2120c1a3052820
Fortis <fortispotens@gmail.com>
2021-01-08 09:34:29 +0100

    Initial commit with express setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const objectToCSV = require('objects-to-csv');
+
+const app = express();
+
+const port = 5000;
+
+app.listen(port, () => console.debug('Welcome to Load json to csv'));
+
+// Node.js Express Download CSV File From Array of Objects Using objects to csv Lib
+// How to Convert CSV Files to JSON Files in Node.js Using CSVTOJSON Library Full E
